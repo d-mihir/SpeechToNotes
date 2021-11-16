d005da06803c348d312c4674fd3efd0a7914e6b3
d-mihir <mihirdeshpande1802@gmail.com>
2021-11-16 19:46:11 +0530

    required

--- first.py
+++ first.py
@@ -1,4 +1,13 @@
-import string
-for i in range(10):
-    print(string.ascii_uppercase)
+import cv2
+capture = cv2.VideoCapture(0)
+while True:
+    ret,frame=capture.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame',frame)
+    cv2.imshow('gray_frame',gray)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cap.release()
+cv2.destoryAllWindows()
 
